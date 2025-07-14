21f3700353f8741438e01c7651de7df0712e2d7a
szuluaga-beep <steven000069@hotmail.com>
2025-07-14 15:11:36 -0500

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityQ10.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        public string FirtsName { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters.")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Email Address")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Identity Document")]
+        public string Document { get; set; } = null!;
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirtsName} {LastName}";
+            }
+        }
+    }
+}
